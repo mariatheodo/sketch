3db5a777aea0113b3f50205c6acc10c9c92024fe
mariatheodo <maria.theodo@gmail.com>
2017-02-07 23:38:44 +0200

    makes new grid

--- scripts/script.js
+++ scripts/script.js
@@ -1,32 +1,37 @@
-$(document).ready(makeGrid(16));
+$(document).ready(function() {
+	for (var i = 0; i < 16; i++) {
+		for (var j = 0; j < 16; j++) {
+			$("#container").append("<div class='square'></div>");
+		}
+	}
+});
 
 $(document).ready(function() {
+	$("#reset").click(function() {
+		$(".square").css("background-color", "white");
+	});
+
+	$("#newGrid").click(function() {
+		var size = prompt("How many squares do you want in the grid?");
+		$("#container").empty();
+		var w = parseInt(960 / size)- 2;
+
+		for (var i = 0; i < size; i++) {
+			for (var j = 0; j < size; j++) {
+				$("#container").append("<div class='square'></div>");
+			}
+		};
+		$(".square").css({
+			width: function(i) {
+				return w;
+			},
+			height: function(i) {
+				return w;
+			}
+		});
+	});
+
 	$(".square").mouseenter(function() {
 		$(this).css("background-color", "grey");
 	});
 });
-
-function reset() {
-	$(".square").css("background-color", "white");
-};
-
-function newGrid() {
-	var size = prompt("How many squares do you want for the grid?");
-	$("#container").empty();
-	makeGrid(size);
-}
-
-function makeGrid(x) {
-	var w = parseInt(960 / x)- 2;
-	$(".square").css("width", function(i) {
-		return w;
-	});
-	$(".square").css("height", function(i) {
-		return w;
-	});
-	for (var i = 0; i < x; i++) {
-		for (var j = 0; j < x; j++) {
-			$("#container").append("<div class='square'></div>");
-		}
-	}
-};
